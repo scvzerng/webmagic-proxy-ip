a06444fd02f0b1e6e1a18f5b1bd4b88ce1510f50
scvzerng <443683059a>
2017-12-04 17:56:23 +0800

    去除过滤失败次数过多的条件

--- src/main/java/com/zero/webmagic/dao/IpRepository.java
+++ src/main/java/com/zero/webmagic/dao/IpRepository.java
@@ -1,8 +1,6 @@
 package com.zero.webmagic.dao;
 
 import com.zero.webmagic.entity.Ip;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -22,8 +20,8 @@ public interface IpRepository extends CrudRepository<Ip,Long>,PagingAndSortingRe
     Ip findByIp(String ip);
     Ip findByIpAndPort(String ip,Integer port);
 
-    @Query(nativeQuery = true,value = "select * from ip where ip.can_use=true and ip.fail_count<30 limit :start,1 ")
+    @Query(nativeQuery = true,value = "select * from ip where ip.can_use=true limit :start,1 ")
     Ip randomQualityIp(@Param("start") int offset);
-    @Query(nativeQuery = true,value = "select count(*) from ip where ip.can_use=true and ip.fail_count<30")
+    @Query(nativeQuery = true,value = "select count(*) from ip where ip.can_use=true")
     long queryQualityCount();
 }
